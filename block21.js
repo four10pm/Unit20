f2048145172922a3ff4d04f50a16569fe143df9a
Paulina <paulinasfielek@gmail.com>
2023-11-29 13:37:17 -0800

    fixed date/time display & added non-functional form

--- block21.js
+++ block21.js
@@ -1,4 +1,7 @@
 // A user enters the website and finds a list of the names, dates, times, locations, and descriptions of all the parties that are happening. // 
+// Next to each party in the list is a delete button. The user clicks the delete button for one of the parties. That party is then removed from the list.
+// There is also a form that allows the user to enter information about a new party that they want to schedule. After filling out the form and submitting it, the user observes their party added to the list of parties.
+
 
 // API call // 
 const API = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/2310-GHP-ET-WEB-FT-SF/events`
@@ -10,6 +13,7 @@ const state = {
 
 // references pointing to my HTML // 
 const eventsList = document.querySelector("#eventsList");
+const deleteButton = document.querySelector(".delete");
 
 // initial render - run the functions to get & display API info // 
 async function render() {
@@ -31,15 +35,28 @@ async function getEvents() {
 
 };
 
+// date and time //
+
 // display API info // 
 function renderEvents() { 
+    if (state.events.length === 0) {
+        const element = document.createElement("li");
+        element.innerHTML = `Sorry, there are no parties planned.`
+        element.style.listStyle = "none";
+        eventsList.replaceChildren(element);
+        return element;
+    };
+
     const eventsData = state.events.map((event) => {
+        const date = event.date.split("T")[0];
+        const time = event.date.split("T")[1].split(".")[0];
         const element = document.createElement("li");
         element.innerHTML = 
             `<h3> ${event.name} </h3>
-            <h4> ${event.date} </h4>
+            <h4> ${date} ${time} </h4>
             <h5> ${event.location} </h5>
-            <p> ${event.description} </p>`;
+            <p> ${event.description} </p>
+            <button class="delete" id=${event.id}> DELETE </button> ` ;
         element.style.border = "black 1px solid";
         element.style.listStyle = "none";
         element.style.padding = "3px";
@@ -47,3 +64,5 @@ function renderEvents() {
     });
     eventsList.replaceChildren(...eventsData);
 };
+
+
